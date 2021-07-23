9308973207961511d4b613683f27bcb28f345eb1
qwshang <qws5281@163.com>
2021-07-23 16:35:35 +0800

    项目创建

---
+++ src/com/qws/one/TestOne.java
@@ -0,0 +1,12 @@
+package com.qws.one;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
